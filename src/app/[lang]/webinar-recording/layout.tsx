7d01571aaf6f20d3029a09bf6dc00c07bce46d1d
Ayomide Oluniyi <ayorg360@gmail.com>
2025-05-09 18:18:26 +0300

    Update webinar recording layout title for clarity
    - Changed the title in the SEO metadata from 'Video Archive' to 'Hibarr Webinar Access Form' to better reflect the content and purpose of the page.

--- src/app/[lang]/webinar-recording/layout.tsx
+++ src/app/[lang]/webinar-recording/layout.tsx
@@ -2,7 +2,7 @@ import { Metadata } from 'next'
 import { generateSEOMetadata } from '@/lib/utils'
 
 export async function generateMetadata(): Promise<Metadata> {
-  return generateSEOMetadata(undefined, { title: 'Video Archive' })
+  return generateSEOMetadata(undefined, { title: 'Hibarr Webinar Access Form' })
 }
 
 export default function BankingPackagesLayout({ children }: { children: React.ReactNode }) {
