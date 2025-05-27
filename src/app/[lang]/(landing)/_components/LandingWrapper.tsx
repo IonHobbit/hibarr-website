b19b53e03fe6d04e8bf97507ecb62c69b231a1a3
Ayomide Oluniyi <ayorg360@gmail.com>
2025-05-27 13:06:28 +0300

    Refactor LandingWrapper component to comment out feature flag logic
    - Commented out the feature flag logic and related imports in the LandingWrapper component for future implementation.
    - Simplified the return structure to focus on the default LandingSection rendering while retaining the commented code for potential reactivation.

--- src/app/[lang]/(landing)/_components/LandingWrapper.tsx
+++ src/app/[lang]/(landing)/_components/LandingWrapper.tsx
@@ -1,10 +1,10 @@
 "use client"
 
-import Image from 'next/image'
-import AnimatedLandingSection from './AnimatedLandingSection'
 import LandingSection from './LandingSection'
 import { HomePage } from '@/types/sanity.types'
-import { useFeatureFlagVariantKey } from 'posthog-js/react'
+// import Image from 'next/image'
+// import AnimatedLandingSection from './AnimatedLandingSection'
+// import { useFeatureFlagVariantKey } from 'posthog-js/react'
 
 type LandingWrapperProps = {
   data: HomePage
@@ -15,25 +15,23 @@ export default function LandingWrapper({ data }: LandingWrapperProps) {
   // const distinctId = getUserDistinctId();
   // const landingSectionFlagKey = await posthog.getFeatureFlag('landing-section', distinctId)
 
-  const landingSectionFlagKey = useFeatureFlagVariantKey('landing-section')
+  // const landingSectionFlagKey = useFeatureFlagVariantKey('landing-section')
 
-  console.log(landingSectionFlagKey)
+  // if (landingSectionFlagKey === undefined) {
+  //   return (
+  //     <div className='flex justify-center items-center h-screen'>
+  //       <Image src='/logos/logo-blue.png' className='animate-pulse' alt='logo' width={250} height={50} />
+  //     </div>
+  //   )
+  // }
 
-  if (landingSectionFlagKey === undefined) {
-    return (
-      <div className='flex justify-center items-center h-screen'>
-        <Image src='/logos/logo-blue.png' className='animate-pulse' alt='logo' width={250} height={50} />
-      </div>
-    )
-  }
+  // if (landingSectionFlagKey === 'animated') {
+  //   return <AnimatedLandingSection data={data} />
+  // }
 
-  if (landingSectionFlagKey === 'animated') {
-    return <AnimatedLandingSection data={data} />
-  }
-
-  if (landingSectionFlagKey === 'control') {
-    return <LandingSection data={data} />
-  }
+  // if (landingSectionFlagKey === 'control') {
+  //   return <LandingSection data={data} />
+  // }
 
   return <LandingSection data={data} />
 }
