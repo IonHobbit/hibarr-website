f97f83027a3a6b104a7f2be124c308263a727784
Ayomide Oluniyi <ayorg360@gmail.com>
2025-06-12 14:39:52 +0300

    Refactor ConsultationProcessSection and InvestorCommunitySection components for improved layout and responsiveness
    - Updated the layout of ConsultationProcessSection to enhance flex behavior on larger screens.
    - Modified InvestorCommunitySection to use a column layout for better spacing and organization.
    - Added an Image import in InvestorCommunitySection for potential future use.

--- src/app/[lang]/(landing)/_components/ConsultationProcessSection.tsx
+++ src/app/[lang]/(landing)/_components/ConsultationProcessSection.tsx
@@ -13,11 +13,11 @@ export default function ConsultationProcessSection({ data }: ConsultationProcess
           <h3 className='text-3xl md:text-5xl text-center text-primary-foreground'>{data?.title}</h3>
           <p className="text-sm md:text-lg text-primary-foreground/80 text-center">{data?.description}</p>
         </div>
-        <div className="grid grid-cols-1 md:flex flex-wrap justify-center gap-6">
+        <div className="grid grid-cols-1 lg:flex flex-wrap justify-center gap-6">
           {data?.steps?.map((step, index) => (
             <div
               key={index}
-              className="group overflow-hidden cursor-default flex flex-col gap-4 border rounded-lg p-4 basis-full md:basis-[31.5%] md:nth-last-[-n+2]:basis-[48%] hover:bg-background/5 transition-all duration-300">
+              className="group overflow-hidden cursor-default shrink-0 flex flex-col gap-4 border rounded-lg p-4 basis-full md:basis-[31.5%] md:nth-last-[-n+2]:basis-[48%] hover:bg-background/5 transition-all duration-300">
               <div className="flex flex-wrap items-center gap-4">
                 <Icon icon={step.icon ?? ''} className="text-primary-foreground/80 shrink-0 text-4xl group-hover:scale-110 transition-all duration-300" />
                 <div className="flex flex-col gap-1">
--- src/app/[lang]/(landing)/_components/InvestorCommunitySection.tsx
+++ src/app/[lang]/(landing)/_components/InvestorCommunitySection.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from '@/components/ui/button'
 import { HomePage } from '@/types/sanity.types'
+import Image from 'next/image'
 import Link from 'next/link'
 
 type InvestorCommunitySectionProps = {
@@ -11,13 +12,16 @@ type InvestorCommunitySectionProps = {
 export default function InvestorCommunitySection({ data }: InvestorCommunitySectionProps) {
   return (
     <section id='investor-community' className="section">
-      <div className="flex flex-wrap md:flex-nowrap items-center justify-between gap-10 bg-primary p-6 rounded-lg">
-        <h3 className='text-3xl md:text-5xl text-center md:text-left text-primary-foreground'>Join our community <br /> of Investors now on <span className='text-secondary'>Facebook</span></h3>
-        <Button size="lg" variant="accent" className='w-full md:w-auto font-semibold' asChild>
-          <Link href={data?.CTA?.url ?? ''} target='_blank'>
-            {data?.CTA?.label ?? 'Join Here'}
-          </Link>
-        </Button>
+      <div className="flex flex-col gap-4 bg-primary p-6 rounded-lg">
+        {/* <Image src="/images/smart-investor.png" alt="Smart Investor" width={1000} height={1000} className='w-full h-auto' /> */}
+        <div className="flex flex-wrap md:flex-nowrap items-center justify-between gap-10">
+          <h3 className='text-3xl md:text-5xl text-center md:text-left text-primary-foreground'>Join our community <br /> of Investors now on <span className='text-secondary'>Facebook</span></h3>
+          <Button size="lg" variant="accent" className='w-full md:w-auto font-semibold' asChild>
+            <Link href={data?.CTA?.url ?? ''} target='_blank'>
+              {data?.CTA?.label ?? 'Join Here'}
+            </Link>
+          </Button>
+        </div>
       </div>
     </section>
   )
