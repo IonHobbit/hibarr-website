6998f6e08c1a038155a8fb5b900d464a5458d18d
Ayomide Oluniyi <ayorg360@gmail.com>
2025-08-11 16:27:34 +0300

    Remove unused import of client from LeadershipTeamSection to streamline code and improve clarity.

--- src/app/[lang]/(landing)/_components/LeadershipTeamSection.tsx
+++ src/app/[lang]/(landing)/_components/LeadershipTeamSection.tsx
@@ -1,4 +1,4 @@
-import { client, fetchSanityData } from "@/lib/sanity/client";
+import { fetchSanityData } from "@/lib/sanity/client";
 import { HomePage, Team } from "@/types/sanity.types";
 import Image from "next/image";
 import { generateImageUrl } from "@/lib/utils";
