2f0ae1af99833a155e66702d6c9b78b669b6e547
Rayrsn <25773909+Rayrsn@users.noreply.github.com>
2025-09-16 13:36:33 +0300

    Update image sources in MissionVisionSection for improved accessibility and SEO

--- src/app/[lang]/about/_components/MissionVisionSection.tsx
+++ src/app/[lang]/about/_components/MissionVisionSection.tsx
@@ -12,7 +12,13 @@ export default function MissionVisionSection({ data }: MissionVisionSectionProps
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
         <div className="flex flex-col gap-4 order-2 md:order-1">
           <div className='relative w-full h-96 rounded-lg overflow-hidden'>
-            <Image src="/images/about/about-team-meeting-boardroom.webp" alt="About Vision" fill sizes='100%' className='object-cover' />
+            <Image
+              src="https://res.cloudinary.com/hibarr/image/upload/about-team-meeting-boardroom_xsrpol"
+              alt="Hibarr team meeting in a boardroom, discussing company vision"
+              fill
+              sizes='100%'
+              className='object-cover'
+            />
           </div>
           <h3 className="text-3xl md:text-4xl">
             {data?.vision?.title}
@@ -32,7 +38,13 @@ export default function MissionVisionSection({ data }: MissionVisionSectionProps
             {data?.mission?.content}
           </p>
           <div className='relative w-full h-96 rounded-lg overflow-hidden'>
-            <Image src="/images/about/about-team-collaboration-analytics.webp" alt="About Mission" fill sizes='100%' className='object-cover' />
+            <Image
+              src="https://res.cloudinary.com/hibarr/image/upload/about-team-collaboration-analytics_hvhjrh"
+              alt="Team collaborating while reviewing analytics dashboards"
+              fill
+              sizes='100%'
+              className='object-cover'
+            />
           </div>
         </div>
       </div>
