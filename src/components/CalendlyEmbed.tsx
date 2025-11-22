8bd4751c0c6b03368992bb1384cddbebc6051552
Rayrsn <25773909+Rayrsn@users.noreply.github.com>
2025-11-22 14:14:54 +0200

    Migrate to `useRef` for DOM element access, implement `gsap.context` for animation cleanup, and streamline script appending.

--- src/components/CalendlyEmbed.tsx
+++ src/components/CalendlyEmbed.tsx
@@ -8,13 +8,12 @@ type CalendlyEmbedProps = {
 
 export default function CalendlyEmbed({ url }: CalendlyEmbedProps) {
   useEffect(() => {
-    const head = document.querySelector("head");
     const script = document.createElement("script");
     script.setAttribute(
       "src",
       "https://assets.calendly.com/assets/external/widget.js"
     );
-    head?.appendChild(script);
+    document.head.appendChild(script);
   }, []);
 
   return (
