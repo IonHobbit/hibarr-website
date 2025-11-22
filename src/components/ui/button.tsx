80b20a1a7da599c68d53beef46376fe6bde02604
Ayomide Oluniyi <ayorg360@gmail.com>
2025-11-22 12:24:03 +0200

    Update button component size for improved consistency in large variant

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -31,7 +31,7 @@ const buttonVariants = cva(
       size: {
         default: "min-h-8 px-4 py-2 has-[>svg]:px-3",
         sm: "min-h-7 gap-1.5 px-3 py-2 has-[>svg]:px-2.5",
-        lg: "min-h-13 text-lg font-medium px-6 py-2 uppercase has-[>svg]:px-4",
+        lg: "min-h-14 text-lg font-medium px-6 py-2 uppercase has-[>svg]:px-4",
         icon: "size-9",
       },
       fullWidth: {
