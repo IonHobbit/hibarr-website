48b0d4c7ab036eda4faa3c06a79ff021efc596ea
Ayomide Oluniyi <ayorg360@gmail.com>
2025-12-16 11:44:56 +0200

    Comment out unused landing page sections
    Temporarily disabled several imported components and their usage in the landing page to streamline the rendered content. This may be for development, debugging, or to simplify the page for now.

--- src/app/[lang]/(landing)/page.tsx
+++ src/app/[lang]/(landing)/page.tsx
@@ -1,25 +1,25 @@
 import { Fragment } from 'react';
 import type { Locale } from '@/lib/i18n-config';
 import AboutSection from './_components/AboutSection';
-import PartnersSection from '../_components/PartnersSection';
-import WhyCyprus from './_components/WhyCyprus';
-import LeadershipTeamSection from './_components/LeadershipTeamSection';
-import CallToActionSection from './_components/CallToActionSection';
-import FeaturedSection from '../_components/FeaturedSection';
-import TestimonialsSection from '@/app/[lang]/_components/TestimonialsSection';
+// import PartnersSection from '../_components/PartnersSection';
+// import WhyCyprus from './_components/WhyCyprus';
+// import LeadershipTeamSection from './_components/LeadershipTeamSection';
+// import CallToActionSection from './_components/CallToActionSection';
+// import FeaturedSection from '../_components/FeaturedSection';
+// import TestimonialsSection from '@/app/[lang]/_components/TestimonialsSection';
 import { fetchSanityData } from "@/lib/third-party/sanity.client";
 import { HomePage, SeoMetaFields, Testimonial } from '@/types/sanity.types';
-import ConsultationProcessSection from './_components/ConsultationProcessSection';
-import InvestorCommunitySection from './_components/InvestorCommunitySection';
-import WebinarSection from './_components/WebinarSection';
+// import ConsultationProcessSection from './_components/ConsultationProcessSection';
+// import InvestorCommunitySection from './_components/InvestorCommunitySection';
+// import WebinarSection from './_components/WebinarSection';
 import { Metadata } from 'next';
 import { generateSEOMetadata } from '@/lib/utils';
 import LandingWrapper from './_components/LandingWrapper';
 import { seoTitles } from '@/lib/seo-titles';
 import { seoDescriptions } from '@/data/seo-descriptions';
 import { fetchFiles, CloudinaryFile } from '@/lib/third-party/cloudinary.client';
-import { cookies } from 'next/headers';
-import CaseStudiesSection from './_components/CaseStudiesSection';
+// import { cookies } from 'next/headers';
+// import CaseStudiesSection from './_components/CaseStudiesSection';
 // import MeetRabih from './_components/MeetRabih';
 
 type HomePageProps = {
@@ -39,8 +39,8 @@ export const revalidate = 60;
 export default async function Home(props: HomePageProps) {
   const { lang } = await props.params;
 
-  const cookieStore = await cookies();
-  const disableMedia = cookieStore.get('hibarr_nomedia')?.value === '1';
+  // const cookieStore = await cookies();
+  // const disableMedia = cookieStore.get('hibarr_nomedia')?.value === '1';
 
   const [data, testimonials, partners] = await Promise.all([
     fetchSanityData<HomePage>(`*[_type == "homePage" && language == $lang][0]`, { lang }),
@@ -51,7 +51,7 @@ export default async function Home(props: HomePageProps) {
   return (
     <Fragment>
       <LandingWrapper data={data} lang={lang} />
-      <FeaturedSection />
+      {/* <FeaturedSection /> */}
       {/* <div className='section'>
         <div className='bg-primary rounded-lg p-4 py-8 md:py-4 md:px-2 max-w-screen-sm xl:max-w-screen-xl mx-auto'>
           <SearchBar />
@@ -59,16 +59,16 @@ export default async function Home(props: HomePageProps) {
       </div> */}
       <AboutSection data={data} />
       {/* <FindrSection /> */}
-      <TestimonialsSection lang={lang} data={data} testimonials={testimonials} />
-      <PartnersSection partnersTitle={data?.partnersSection?.title} partners={partners} />
-      <ConsultationProcessSection data={data.consultationProcessSection} />
-      <WebinarSection />
+      {/* <TestimonialsSection lang={lang} data={data} testimonials={testimonials} /> */}
+      {/* <PartnersSection partnersTitle={data?.partnersSection?.title} partners={partners} /> */}
+      {/* <ConsultationProcessSection data={data.consultationProcessSection} /> */}
+      {/* <WebinarSection /> */}
       {/* <WhyCyprus data={data.whyCyprusSection} disableMedia={disableMedia} /> */}
       {/* <CaseStudiesSection data={data.caseStudiesSection} lang={lang} disableMedia={disableMedia} /> */}
-      <InvestorCommunitySection data={data.investorCommunitySection} />
+      {/* <InvestorCommunitySection data={data.investorCommunitySection} /> */}
       {/* <MeetRabih data={data.meetRabihSection} disableMedia={disableMedia} /> */}
-      <LeadershipTeamSection data={data.leadershipTeamSection} />
-      <CallToActionSection data={data.callToActionSection} />
+      {/* <LeadershipTeamSection data={data.leadershipTeamSection} /> */}
+      {/* <CallToActionSection data={data.callToActionSection} /> */}
       {/* <FreebieSignupSection data={data.freebieSignupSection} /> */}
       {/* <SignupSection data={data.freebieSignupSection} /> */}
     </Fragment>
