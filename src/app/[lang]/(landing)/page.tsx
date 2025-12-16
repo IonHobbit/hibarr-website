03472374c25303c7d4edb31ce1890418ce2b8f68
Ayomide Oluniyi <ayorg360@gmail.com>
2025-12-16 11:04:17 +0200

    Enable all landing page sections
    Uncommented and imported all previously disabled sections in the landing page, including About, WhyCyprus, CaseStudies, MeetRabih, CallToAction, and FeaturedSection. Also enabled cookie handling for media display logic.

--- src/app/[lang]/(landing)/page.tsx
+++ src/app/[lang]/(landing)/page.tsx
@@ -1,11 +1,11 @@
 import { Fragment } from 'react';
 import type { Locale } from '@/lib/i18n-config';
-// import AboutSection from './_components/AboutSection';
+import AboutSection from './_components/AboutSection';
 import PartnersSection from '../_components/PartnersSection';
-// import WhyCyprus from './_components/WhyCyprus';
+import WhyCyprus from './_components/WhyCyprus';
 import LeadershipTeamSection from './_components/LeadershipTeamSection';
-// import CallToActionSection from './_components/CallToActionSection';
-// import FeaturedSection from '../_components/FeaturedSection';
+import CallToActionSection from './_components/CallToActionSection';
+import FeaturedSection from '../_components/FeaturedSection';
 import TestimonialsSection from '@/app/[lang]/_components/TestimonialsSection';
 import { fetchSanityData } from "@/lib/third-party/sanity.client";
 import { HomePage, SeoMetaFields, Testimonial } from '@/types/sanity.types';
@@ -18,7 +18,9 @@ import LandingWrapper from './_components/LandingWrapper';
 import { seoTitles } from '@/lib/seo-titles';
 import { seoDescriptions } from '@/data/seo-descriptions';
 import { fetchFiles, CloudinaryFile } from '@/lib/third-party/cloudinary.client';
-// import { cookies } from 'next/headers';
+import { cookies } from 'next/headers';
+import CaseStudiesSection from './_components/CaseStudiesSection';
+import MeetRabih from './_components/MeetRabih';
 
 type HomePageProps = {
   params: Promise<{ lang: Locale }>;
@@ -37,8 +39,8 @@ export const revalidate = 60;
 export default async function Home(props: HomePageProps) {
   const { lang } = await props.params;
 
-  // const cookieStore = await cookies();
-  // const disableMedia = cookieStore.get('hibarr_nomedia')?.value === '1';
+  const cookieStore = await cookies();
+  const disableMedia = cookieStore.get('hibarr_nomedia')?.value === '1';
 
   const [data, testimonials, partners] = await Promise.all([
     fetchSanityData<HomePage>(`*[_type == "homePage" && language == $lang][0]`, { lang }),
@@ -49,24 +51,24 @@ export default async function Home(props: HomePageProps) {
   return (
     <Fragment>
       <LandingWrapper data={data} lang={lang} />
-      {/* <FeaturedSection /> */}
+      <FeaturedSection />
       {/* <div className='section'>
         <div className='bg-primary rounded-lg p-4 py-8 md:py-4 md:px-2 max-w-screen-sm xl:max-w-screen-xl mx-auto'>
           <SearchBar />
         </div>
       </div> */}
-      {/* <AboutSection data={data} /> */}
+      <AboutSection data={data} />
       {/* <FindrSection /> */}
       <TestimonialsSection lang={lang} data={data} testimonials={testimonials} />
       <PartnersSection partnersTitle={data?.partnersSection?.title} partners={partners} />
       <ConsultationProcessSection data={data.consultationProcessSection} />
       <WebinarSection />
-      {/* <WhyCyprus data={data.whyCyprusSection} disableMedia={disableMedia} /> */}
-      {/* <CaseStudiesSection data={data.caseStudiesSection} lang={lang} disableMedia={disableMedia} /> */}
+      <WhyCyprus data={data.whyCyprusSection} disableMedia={disableMedia} />
+      <CaseStudiesSection data={data.caseStudiesSection} lang={lang} disableMedia={disableMedia} />
       <InvestorCommunitySection data={data.investorCommunitySection} />
-      {/* <MeetRabih data={data.meetRabihSection} disableMedia={disableMedia} /> */}
+      <MeetRabih data={data.meetRabihSection} disableMedia={disableMedia} />
       <LeadershipTeamSection data={data.leadershipTeamSection} />
-      {/* <CallToActionSection data={data.callToActionSection} /> */}
+      <CallToActionSection data={data.callToActionSection} />
       {/* <FreebieSignupSection data={data.freebieSignupSection} /> */}
       {/* <SignupSection data={data.freebieSignupSection} /> */}
     </Fragment>
