8420934f1902e0dea70fcba69279883f504c1dc9
Ayomide Oluniyi <ayorg360@gmail.com>
2025-12-16 10:45:33 +0200

    Comment out unused sections in landing page for improved layout clarity

--- src/app/[lang]/(landing)/page.tsx
+++ src/app/[lang]/(landing)/page.tsx
@@ -2,7 +2,7 @@ import { Fragment } from 'react';
 import type { Locale } from '@/lib/i18n-config';
 import AboutSection from './_components/AboutSection';
 import PartnersSection from '../_components/PartnersSection';
-import WhyCyprus from './_components/WhyCyprus';
+// import WhyCyprus from './_components/WhyCyprus';
 import LeadershipTeamSection from './_components/LeadershipTeamSection';
 import CallToActionSection from './_components/CallToActionSection';
 import FeaturedSection from '../_components/FeaturedSection';
@@ -14,11 +14,11 @@ import InvestorCommunitySection from './_components/InvestorCommunitySection';
 import WebinarSection from './_components/WebinarSection';
 import { Metadata } from 'next';
 import { generateSEOMetadata } from '@/lib/utils';
-import LandingWrapper from './_components/LandingWrapper';
+// import LandingWrapper from './_components/LandingWrapper';
 import { seoTitles } from '@/lib/seo-titles';
 import { seoDescriptions } from '@/data/seo-descriptions';
 import { fetchFiles, CloudinaryFile } from '@/lib/third-party/cloudinary.client';
-import { cookies } from 'next/headers';
+// import { cookies } from 'next/headers';
 
 type HomePageProps = {
   params: Promise<{ lang: Locale }>;
@@ -37,8 +37,8 @@ export const revalidate = 60;
 export default async function Home(props: HomePageProps) {
   const { lang } = await props.params;
 
-  const cookieStore = await cookies();
-  const disableMedia = cookieStore.get('hibarr_nomedia')?.value === '1';
+  // const cookieStore = await cookies();
+  // const disableMedia = cookieStore.get('hibarr_nomedia')?.value === '1';
 
   const [data, testimonials, partners] = await Promise.all([
     fetchSanityData<HomePage>(`*[_type == "homePage" && language == $lang][0]`, { lang }),
@@ -48,7 +48,7 @@ export default async function Home(props: HomePageProps) {
 
   return (
     <Fragment>
-      <LandingWrapper data={data} lang={lang} disableMedia={disableMedia} />
+      {/* <LandingWrapper data={data} lang={lang} disableMedia={disableMedia} /> */}
       <FeaturedSection />
       {/* <div className='section'>
         <div className='bg-primary rounded-lg p-4 py-8 md:py-4 md:px-2 max-w-screen-sm xl:max-w-screen-xl mx-auto'>
@@ -61,7 +61,7 @@ export default async function Home(props: HomePageProps) {
       <PartnersSection partnersTitle={data?.partnersSection?.title} partners={partners} />
       <ConsultationProcessSection data={data.consultationProcessSection} />
       <WebinarSection />
-      <WhyCyprus data={data.whyCyprusSection} disableMedia={disableMedia} />
+      {/* <WhyCyprus data={data.whyCyprusSection} disableMedia={disableMedia} /> */}
       {/* <CaseStudiesSection data={data.caseStudiesSection} lang={lang} disableMedia={disableMedia} /> */}
       <InvestorCommunitySection data={data.investorCommunitySection} />
       {/* <MeetRabih data={data.meetRabihSection} disableMedia={disableMedia} /> */}
