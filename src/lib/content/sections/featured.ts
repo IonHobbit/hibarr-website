d8c43cf4e1fd60b4606aab47ed3af91aa40515ac
Ayomide Oluniyi <ayorg360@gmail.com>
2025-12-16 19:34:40 +0200

    refactor: comment out logo URLs in FeaturedSection for future updates
    Updated the logos array in the FeaturedSection component to comment out existing logo URLs, preparing the section for potential content updates while maintaining the current structure.

--- src/lib/content/sections/featured.ts
+++ src/lib/content/sections/featured.ts
@@ -7,15 +7,15 @@ type FeaturedContent = {
   }
 }
 
-const logos = [
-  "https://res.cloudinary.com/hibarr/image/upload/v1758785140/whatswhat-logo_xyq9sw.png",
+const logos: string[] = [
+  // "https://res.cloudinary.com/hibarr/image/upload/v1758785140/whatswhat-logo_xyq9sw.png",
   // "https://res.cloudinary.com/hibarr/image/upload/v1750747645/erfolg-magazin-logo_nthlb6.webp",
-  "https://res.cloudinary.com/hibarr/image/upload/v1750747645/gruender-de-logo-black_ndnwry.png",
-  "https://res.cloudinary.com/hibarr/image/upload/v1750747645/forbes-logo_vwad88.png",
-  "https://res.cloudinary.com/hibarr/image/upload/v1750747645/wallstreet-online-logo-black-300x91-1_gelaga.png",
-  "https://res.cloudinary.com/hibarr/image/upload/v1750747645/Black-Netflix-Text-Logo_ehsqkh.png",
-  "https://res.cloudinary.com/hibarr/image/upload/v1750747645/suddeutsche-zeitung-logo_sqjx2p.png",
-  "https://res.cloudinary.com/hibarr/image/upload/v1750747645/bellevue-logo-black_ej8bvy.png"
+  // "https://res.cloudinary.com/hibarr/image/upload/v1750747645/gruender-de-logo-black_ndnwry.png",
+  // "https://res.cloudinary.com/hibarr/image/upload/v1750747645/forbes-logo_vwad88.png",
+  // "https://res.cloudinary.com/hibarr/image/upload/v1750747645/wallstreet-online-logo-black-300x91-1_gelaga.png",
+  // "https://res.cloudinary.com/hibarr/image/upload/v1750747645/Black-Netflix-Text-Logo_ehsqkh.png",
+  // "https://res.cloudinary.com/hibarr/image/upload/v1750747645/suddeutsche-zeitung-logo_sqjx2p.png",
+  // "https://res.cloudinary.com/hibarr/image/upload/v1750747645/bellevue-logo-black_ej8bvy.png"
 ]
 
 export const featuredContent: FeaturedContent = {
